e3273210c79d13d5ac3715363887631e56c65d13
SAADMAN SAKIB <saadmansakib621@gmail.com>
2026-06-08 18:25:50 +0600

    Setup: rename package to numcompute_stream, add .gitignore, update pyproject

---
+++ numcompute-stream/__init__.py
@@ -0,0 +1,15 @@
+"""
+NumCompute-Stream: A Modularised Ensemble Tree-based Streaming Machine Learning Framework
+Author: Saadman Sakib
+"""
+
+from . import io
+from . import preprocessing
+from . import stats
+from . import metrics
+from . import pipeline
+from . import utils
+from . import visualise
+from . import tree
+from . import ensemble
+from . import stream
